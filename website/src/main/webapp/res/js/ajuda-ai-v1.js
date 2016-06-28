1c8ad43cbdb347f0736e8b936d972060d975baf9
Rafael g0dkar <rafael.lins777@gmail.com>
2016-06-28 12:14:55 -0300

    Tapa no visual: Página da Instituição

--- website/src/main/webapp/res/js/ajuda-ai-v1.js
+++ website/src/main/webapp/res/js/ajuda-ai-v1.js
@@ -1,7 +1,11 @@
-(function ($, autosize) {
-	$(function () {
-		$("#page-content").addClass("loaded");
-		$(".slider").slick();
-		$("textarea").each(function () { autosize(this); });
-	});
-})(jQuery, autosize);
+try {
+	(function ($, autosize) {
+		$(function () {
+			$("#page-content").addClass("loaded");
+			$(".slider").slick();
+			$("textarea").each(function () { autosize(this); });
+		});
+	})(jQuery, autosize);
+} catch (e) {
+	document.getElementById("page-content").className = "loaded";
+}
