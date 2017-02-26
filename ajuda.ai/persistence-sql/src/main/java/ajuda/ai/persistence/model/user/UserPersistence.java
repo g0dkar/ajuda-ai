be6b46647603f7fc8838d32e65009e3e6eafbf67
Rafael g0dkar <rafael.lins777@gmail.com>
2017-02-26 17:07:20 -0300

    Persistência de Conf, User e Documentação

--- ajuda.ai/persistence-sql/src/main/java/ajuda/ai/persistence/model/user/UserPersistence.java
+++ ajuda.ai/persistence-sql/src/main/java/ajuda/ai/persistence/model/user/UserPersistence.java
@@ -4,20 +4,25 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.Query;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import ajuda.ai.model.user.User;
 import ajuda.ai.persistence.Persistence;
 import ajuda.ai.persistence.PersistenceService;
+import ajuda.ai.persistence.model.ConfigurationPersistence;
 
 @RequestScoped
 public class UserPersistence implements Persistence<User> {
 	private final PersistenceService ps;
+	private final ConfigurationPersistence conf;
 	
 	/** @deprecated CDI */ @Deprecated
-	UserPersistence() { this(null); }
+	UserPersistence() { this(null, null); }
 	
 	@Inject
-	public UserPersistence(PersistenceService ps) {
+	public UserPersistence(PersistenceService ps, ConfigurationPersistence conf) {
 		this.ps = ps;
+		this.conf = conf;
 	}
 
 	@Override
@@ -27,11 +32,22 @@ public class UserPersistence implements Persistence<User> {
 	
 	@Override
 	public void persist(User object) {
+		if (!object.getPassword().matches("\\$[0-9a-zA-Z]{2}\\$\\d{1,2}\\$.+")) {
+			object.setPassword(BCrypt.hashpw(object.getPassword(), BCrypt.gensalt(conf.get("bcrypt.log_rounds", 10))));
+		}
+		
 		ps.persist(object);
 	}
-
+	
 	@Override
 	public User merge(User object) {
+		if (object.getPassword() == null) {
+			object.setPassword((String) ps.createQuery("SELECT password FROM User WHERE id = :id").setParameter("id", object.getId()).getSingleResult());
+		}
+		else if (!object.getPassword().matches("\\$[0-9a-zA-Z]{2}\\$\\d{1,2}\\$.+")) {
+			object.setPassword(BCrypt.hashpw(object.getPassword(), BCrypt.gensalt(conf.get("bcrypt.log_rounds", 10))));
+		}
+		
 		return ps.merge(object);
 	}
 
